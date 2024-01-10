7147042c445ecb856368d4fa1fb10d8b007d8336
SunaCosmin <147055635+SunaCosmin@users.noreply.github.com>
2024-01-10 20:39:56 +0200

    Add files via upload

--- decoder.py
+++ decoder.py
@@ -5,6 +5,10 @@ class decoder:
 
     def decode_connect(packet):
         bytearray_login = bytearray(packet)
+
+        keep_alive_bytes = packet[10:12]
+        print(keep_alive_bytes)
+        keep_alive_seconds = int.from_bytes(keep_alive_bytes, byteorder='big')
         username_len = bytearray_login[37]
 
         password_len = bytearray_login[37 + username_len + 2]
@@ -16,10 +20,13 @@ class decoder:
         password_start = username_end + 2
         password_end = password_start + password_len
         password = bytearray_login[password_start:password_end].decode("utf-8")
+        return username, password,keep_alive_seconds
 
-        return username, password
+    def decode_publish_packet(publish_packet):
+        topic = publish_packet[15:18].decode('utf-8')
+        qos = int.from_bytes(publish_packet[20:21], byteorder='big')
+        return topic, qos
 
-    def connac_packet(packet):
-        if packet == b'\xc0\x00':
-            return True
-        return False
+
+
+
